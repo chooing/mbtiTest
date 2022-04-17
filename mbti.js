55834e2320070b40943ae387f4fa1d53b92839c2
chooing <chumil7432@gmail.com>
2022-04-17 23:29:26 +0900

    js 수정 및 html 파일 이름 변경

--- mbti.js
+++ mbti.js
@@ -5,8 +5,22 @@ const question = document.getElementById("question");
 const btnA = document.getElementById("btnA");
 const btnB = document.getElementById("btnB");
 
+const mbtiResult = document.getElementById("mbtiResult");
+const btnRestart = document.getElementById("btnRestart");
+
 
 startBtn.addEventListener("click",(e)=>{
     main.classList.add("close");
     question.classList.remove("close");
+});
+
+// 이건 지우기
+btnB.addEventListener("click",(e)=>{
+    question.classList.add("close");
+    mbtiResult.classList.remove("close");
+});
+
+btnRestart.addEventListener("click",(e)=>{
+    mbtiResult.classList.add("close");
+    main.classList.remove("close");
 });
